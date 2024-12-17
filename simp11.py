28ef749a8cce35ff8a2cc59305b535e6031b18a9
XhashimX <90442097+XhashimX@users.noreply.github.com>
2024-12-17 18:32:19 +0300

    Add files via upload

---
+++ simp11.py
@@ -0,0 +1,36 @@
+import sys
+from telegram import Bot
+import os
+import asyncio
+
+# التوكين الخاص بالبوت
+TOKEN = '7871971330:AAFW4V_1Tm1R-qe2JCnOOg0_PYfr59UA9q8'
+bot = Bot(token=TOKEN)
+
+# قراءة مسارات الملفات من سطر الأوامر
+files_to_upload = sys.argv[1:]
+
+# معرف المجموعة
+chat_id = "-4772916658"
+
+# دالة غير متزامنة لإرسال الصورة إلى تليجرام
+async def send_photo_to_telegram(file_path):
+    try:
+        with open(file_path, 'rb') as photo:
+            message = await bot.send_photo(chat_id=chat_id, photo=photo, caption=os.path.basename(file_path))
+            print(f"Photo {file_path} sent successfully!")
+            with open('id.txt', 'a') as f:
+                f.write(f"{message.message_id}\n")
+        # حذف الصورة بعد إرسالها بنجاح
+        os.remove(file_path)
+        print(f"Photo {file_path} deleted successfully!")
+    except Exception as e:
+        print(f"Error sending photo {file_path}: {e}")
+
+# الدالة الرئيسية لتشغيل الكود غير المتزامن
+async def main():
+    for file in files_to_upload:
+        await send_photo_to_telegram(file)
+
+# تشغيل الحدث غير المتزامن
+asyncio.run(main())
